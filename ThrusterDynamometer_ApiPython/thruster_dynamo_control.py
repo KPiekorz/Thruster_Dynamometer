c34733da044a371e76dcc82275b0f73ddb78fa38
Krzysztof Piekorz <piekorz@student.agh.edu.pl>
2020-11-15 03:37:16 +0100

    python som finish

--- ThrusterDynamometer_ApiPython/thruster_dynamo_control.py
+++ ThrusterDynamometer_ApiPython/thruster_dynamo_control.py
@@ -1,7 +1,13 @@
 from threading import Thread
 import serial
+from matplotlib import pyplot as plt
 
 THRUSTER_SERIAL_PORT = 'COM8'
+
+# thruster module id
+TENSOMETER_MODULE_ID = (0x01)
+tenso_data_size = 0
+tenso_data = []
 
 ############## app helper functions #########
 
@@ -13,20 +19,27 @@ THRUSTER_SERIAL_PORT = 'COM8'
 
 ############## app data center ###############
 
-def tensometer_data():
+def tensometer_data(message):
     print("\nTensometer data.")
+    if (len(tenso_data) < tenso_data_size):
+        tenso_value = message[2]
+        tenso_value |= message[3]<<8
+        tenso_data.append(tenso_value)
 
-def data_center(i):
+def data_center(i, message):
     switcher={
             1:tensometer_data,
             }
     func=switcher.get(i, lambda :'Invalid')
-    return func()
+    return func(message)
 
 ############## app control center ############
 
 def tensometer_offset():
     print("\nTensometer offset.")
+    offset = int(input("Enter offset :> "))
+    tenso_message = [TENSOMETER_MODULE_ID, 2, offset&0xFF, offset>>8]
+    print(tenso_message)
 
 def tensometer_calib():
     print("\nTensometer calibration.")
@@ -34,11 +47,31 @@ def tensometer_calib():
 def tensometer_set_rate():
     print("\nTensometer set rate.")
 
+def tensometer_plot():
+    print("\nTensometer set rate.")
+    # open plot dispaly
+    x = list(range(0, tenso_data_size))
+    y = tenso_data
+    plt.plot(x, y)
+    plt.savefig("tenso_plot.png")
+
+def tensometer_get_measurement():
+    print("\nTensometer get measurement.")
+    tenso_data_size = int(input("Enter :> "))
+    tenso_data.clear()
+
+    # receive serial thread 
+    stop_threads = False
+    thread_serial_receive = Thread(target = thread_receive_messages)
+    thread_serial_receive.start()
+
 def control_center(i):
     switcher={
             1:tensometer_offset,
             2:tensometer_calib,
             3:tensometer_set_rate,
+            4:tensometer_plot,
+            5:tensometer_get_measurement,
             }
     func=switcher.get(i, lambda :'Invalid')
     return func()
@@ -46,12 +79,13 @@ def control_center(i):
 ############### app thread ##################
 
 def thread_receive_messages():
+
     while True:
         if (stop_threads == True):
             break
         serial_message = thruster_serial.read()
         if (len(serial_message) != 0):
-            data_center(int(serial_message[0]))
+            data_center(int(serial_message[0]), serial_message)
 
 def thread_dispaly_menu():
      while True:
@@ -60,6 +94,7 @@ def thread_dispaly_menu():
 1 - Tensometer offset;
 2 - Tensometer calibration;
 3 - Tensometer set rate;
+4 - Plot tensometer data;
 """, end='')
         control_num = int(input("Enter :> "))
         if (control_num == 0):
@@ -71,7 +106,7 @@ def thread_dispaly_menu():
 if __name__ == "__main__":
 
     # open file save 
-
+    tenso_file = open("tenso.txt", "w")
     
     # open serial port
     thruster_serial = serial.Serial(
@@ -82,11 +117,6 @@ if __name__ == "__main__":
     bytesize=serial.EIGHTBITS,
     timeout=1
     )
-
-    # receive serial thread 
-    stop_threads = False
-    thread_serial_receive = Thread(target = thread_receive_messages)
-    thread_serial_receive.start()
 
     # send command thread
     thread_seial_send = Thread(target = thread_dispaly_menu)
@@ -100,6 +130,6 @@ if __name__ == "__main__":
     # thread_serial_receive.terminate()
 
     # close serial port and file save
-
+    tenso_file.close()
 
     print("app finished...exiting")
